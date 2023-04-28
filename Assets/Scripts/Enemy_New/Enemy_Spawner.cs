e133910cb5b217e86dd6b6a99ce50d4699f25151
thiaguitoONtop <thiagogarciadecarlo@itisjt.edu.ar>
2023-04-28 12:47:31 -0300

    Update Enemy_Spawner.cs

--- Assets/Scripts/Enemy_New/Enemy_Spawner.cs
+++ Assets/Scripts/Enemy_New/Enemy_Spawner.cs
@@ -2,22 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Spawner : MonoBehaviour
-{
-    var Enemy:Transform;
-private var Timer:float;
- 
-function Awake()
-    {
-        Timer = Time.time + 15;// THIS (15) IS THE SECONDS (TIME) IN WHICH THE ENEMY IS GOING TO SPAWN FOR THE FIRST TIME 
-    }
+public class Enemy_Spawner : MonoBehaviour {
+    [Serailzefield]
+    private GameObject prefacEnemy;
+    private GameObject _enemy;
 
-    function Update()
-    {
-        if (Timer < Time.time)
-        { //This checks wether real time has caught up to the timer
-            Instantiate(Enemy, transform.position, transform.rotation); //This spawns the emeny
-            Timer = Time.time + 15; // THIS (15) IS THE SECONDS (TIME) IN WHICH THE ENEMY IS GOING TO SPAWN FOR THE SECOND TIME 
+    void Uodate
+        {
+            if(_enemy==null)
+            {
+                _enemy = instantiate(prefacEnemy) as GameObject;
+                _enemy.transform.position = new vector3(0,1,4);
+                float angulo = Randos.Rage(0, 360);
+                _enemy.Trensform.Rotate(0,angulo,o)
+            }
         }
-    }
+
 }
