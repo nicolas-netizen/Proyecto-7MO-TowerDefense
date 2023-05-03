c28b36b9092ddfa97ea034953da21cdedc544b04
thiaguitoONtop <thiagogarciadecarlo@itisjt.edu.ar>
2023-05-03 09:48:01 -0300

    enemy_follow

---
+++ Assets/Scripts/Player_E/Player_test_movent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_test_movent : MonoBehaviour
+{
+    public float horizontalMove:
+    public float verticalMove:
+    public CharacterController player;
+
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
