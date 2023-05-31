5ea98d5387278707b4b20b757a66072f91b6eb90
nicolas-netizen <129511169+nicolas-netizen@users.noreply.github.com>
2023-05-31 14:18:26 -0300

    muevo

--- Assets/Scripts/Player_New/ChargeAbility.cs
+++ Assets/Scripts/Player_New/ChargeAbility.cs
@@ -2,81 +2,28 @@ using UnityEngine;
 
 public class ChargeAbility : MonoBehaviour
 {
-    public float chargeSpeed = 10f;
-    public float chargeDuration = 1f;
-    public float chargeCooldown = 5f;
-    public float damage = 10f;
-    public float knockbackForce = 100f;
-    public GameObject chargeEffectPrefab; // Prefab del efecto de carga
+    public float damage = 20f; // Daño causado por la habilidad
+    public float knockbackForce = 10f; // Fuerza de empuje hacia atrás
 
-    private Rigidbody rb;
-    private bool isCharging = false;
-    private bool isCooldown = false;
-    private GameObject chargeEffect; // Referencia al efecto de carga actual
-
-    private void Start()
+    public void DefensiveCharge()
     {
-        rb = GetComponent<Rigidbody>();
-    }
+        // Detectar los enemigos en el área frontal
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position + transform.forward, 3f); // Ajusta el radio según tus necesidades
 
-    private void Update()
-    {
-        if (!isCooldown && Input.GetKeyDown(KeyCode.R))
+        // Aplicar daño y empuje a los enemigos
+        foreach (Collider hitCollider in hitColliders)
         {
-            ActivateAbility();
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (isCharging)
-        {
-            rb.velocity = transform.forward * chargeSpeed;
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (isCharging && other.CompareTag("Enemy"))
-        {
-            Enemy enemy = other.GetComponent<Enemy>();
+            Enemy enemy = hitCollider.GetComponent<Enemy>();
             if (enemy != null)
             {
-                Vector3 knockbackDirection = other.transform.position - transform.position;
-                knockbackDirection.Normalize();
-                enemy.TakeDamage(damage, knockbackDirection);
+                // Aplicar daño al enemigo
+                enemy.TakeDamage(damage);
+
+                // Empujar al enemigo hacia atrás
+                Vector3 knockbackDirection = (enemy.transform.position - transform.position).normalized;
+                enemy.ApplyKnockback(knockbackDirection, knockbackForce);
             }
         }
-    }
-
-    private void ActivateAbility()
-    {
-        isCharging = true;
-
-        // Crear el efecto de carga
-        chargeEffect = Instantiate(chargeEffectPrefab, transform.position, Quaternion.identity);
-
-        // Iniciar la duración de la carga
-        Invoke("StopCharging", chargeDuration);
-    }
-
-    private void StopCharging()
-    {
-        isCharging = false;
-        rb.velocity = Vector3.zero;
-
-        // Destruir el efecto de carga
-        Destroy(chargeEffect);
-
-        // Iniciar la rutina de enfriamiento de la habilidad
-        StartCoroutine(CooldownRoutine());
-    }
-
-    private System.Collections.IEnumerator CooldownRoutine()
-    {
-        isCooldown = true;
-        yield return new WaitForSeconds(chargeCooldown);
-        isCooldown = false;
     }
 }
 
