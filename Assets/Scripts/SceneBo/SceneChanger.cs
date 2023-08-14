0f58e65a64119ef3bc3f09b434e8f44dc98e1329
nicolas-netizen <129511169+nicolas-netizen@users.noreply.github.com>
2023-08-14 11:52:08 -0300

    aa

--- Assets/Scripts/SceneBo/SceneChanger.cs
+++ Assets/Scripts/SceneBo/SceneChanger.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class SceneChanger : MonoBehaviour
 {
     public string sceneName;
+    public Animator fadeAnimator; 
+    [SerializeField] private RawImage _imgFade;
+
+
+    public void PlayWithFade()
+    {
+        _imgFade.GetComponent<Animator>().Play("FadeOut");
+    }
+
     public void ChangeScene()
     {
         SceneManager.LoadScene(sceneName);
