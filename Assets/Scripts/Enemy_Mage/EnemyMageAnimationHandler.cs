0c767a31ea3ff0e98d7a350df943ea49e847baa1
nicolas-netizen <129511169+nicolas-netizen@users.noreply.github.com>
2023-09-01 13:27:30 -0300

    Projectile

--- Assets/Scripts/Enemy_Mage/EnemyMageAnimationHandler.cs
+++ Assets/Scripts/Enemy_Mage/EnemyMageAnimationHandler.cs
@@ -1,21 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMageAnimationHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     [SerializeField] private Animator _animator;
-    [SerializeField] private EnemyM _enemy;
+    [SerializeField] private ProjectileMage _projectilePrefab;
+    [SerializeField] private Transform _projectileSpawnPoint;
+    [SerializeField] private float _projectileSpeed = 10f;
 
     public void StartAttack()
     {
-        _enemy.Movement = true;
+        _animator.SetBool("IsAttacking", true);
     }
 
     public void StopAttack()
     {
-        _enemy.Movement = false;
+        _animator.SetBool("IsAttacking", false);
+    }
+
+    public void ShootProjectile()
+    {
+        if (_projectilePrefab != null && _projectileSpawnPoint != null)
+        {
+            ProjectileMage newProjectile = Instantiate(_projectilePrefab, _projectileSpawnPoint.position, _projectileSpawnPoint.rotation);
+            Rigidbody projectileRigidbody = newProjectile.GetComponent<Rigidbody>();
+
+            if (projectileRigidbody != null)
+            {
+                projectileRigidbody.velocity = _projectileSpawnPoint.forward * _projectileSpeed;
+            }
+        }
     }
 }
